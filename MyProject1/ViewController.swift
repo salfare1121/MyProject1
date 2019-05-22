7f57362b5fc2a12a85837f00a3c506ca1e5ac525
sal <sal@keio.sfc.wide.ad.jp>
2019-05-22 17:42:51 +0900

    Initial Commit

---
+++ MyProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProject1
+//
+//  Created by salfare on 2019/05/22.
+//  Copyright © 2019 sal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
