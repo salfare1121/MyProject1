85375c9aa90c2a0ca21d80600ebd5bb03c5899fd
salfare1121 <salfare1121@gmail.com>
2019-05-24 12:13:12 +0900

    test

---
+++ MyProject1/TaskMakerPage.swift
@@ -0,0 +1,30 @@
+//
+//  TaskMakerPage.swift
+//  MyProject1
+//
+//  Created by salfare on 2019/05/22.
+//  Copyright © 2019 sal. All rights reserved.
+//
+
+import UIKit
+
+class TaskMakerPage: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
